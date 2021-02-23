929e7305dac1e8a88db5a6fd2b7deb9c980fb8b3
Dean Mahaffey <kelly.mahaffey@icloud.com>
2021-02-22 20:20:22 -0800

    updated the javascript and readme file.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # password-generator-HM
+
+This project is a password generator; it is a useful tool to generate a strong password to your liking. This project was built because I was tired of coming up with my passwords. This project will help you develop a password that will include Uppercase, Lowercase, Numbers, and Symbols that can be 8-128 characters long. Learning Javascript is complicated. Doing this project helped me understand just how each function and how DOM Elements work together. 
+
--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-//functions to generator random numbers, symbols, upper and lowercase letters
-
-function getLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getUppercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getNumbers() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getSymbols() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-
-var randomChar = {
-  lower: getLowercase,
-  upper: getUppercase,
-  number: getNumbers,
-  symbols: getSymbols
-};
-
 //DOM Elements
 var passwordEl = document.getElementById('password');
 var lengthEl = document.getElementById('inputNumber');
@@ -32,37 +7,77 @@ var numberEl = document.getElementById('includeNumbers');
 var symbolsEl = document.getElementById('includeSymbols');
 var generateButtonEl = document.getElementById('generate');
 
-//generate password eventListener
-generateButtonEl.addEventListener('click', () => {
-  var lengthInput = parseFloat(lengthEl.value);
-  var lowerChecked = lowercaseEl.checked;
-  var upperChecked = uppercaseEl.checked;
-  var numbersChecked = numberEl.checked;
-  var symbolsChecked = symbolsEl.checked;
-
-
-
-  passwordEl.value = generatePassword(lengthInput, lowerChecked, upperChecked, symbolsChecked, numbersChecked);
-
-
-});
-
-//Generate password
-function generatePassword(lengthInput, upper, number, symbols) {
-  let randomChar = getLowercase()
-  if (upper) randomChar = randomChar.concat(getUppercase())
-  if (number) randomChar = randomChar.concat(getNumbers())
-  if (symbols) randomChar = randomChar.concat(getSymbols())
-
-  var passwordChar = []
-  for (let i = 0; i < lengthInput; i++) {
-    var characterCode = randomChar[Math.floor(Math.random() * randomChar.length)]
-    passwordChar.push(String.fromCharCode(characterCode))
- 
-    console.log(getNumbers(), getSymbols(), getUppercase(), getLowercase())
-
+//Character Selector
+var randomCharacter =  {
+	letter: function() {
+    return String.fromCharCode(getRandomNumber(26) + 97)
+  },
+	lower: function() {
+  	return this.letter().toLowerCase();
+	},
+  upper: function() {
+    return this.letter().toUpperCase();
+  },
+  number: function () {
+  	return String.fromCharCode(getRandomNumber(10) + 48);
+	},
+  symbol: function() {
+    return String.fromCharCode(getRandomNumber(15) + 33);
   }
-  return passwordChar.join('');
-
 }
 
+function getRandomNumber(upperBound) {
+  return Math.floor(Math.random() * upperBound)
+}
+
+//Generate password
+function generatePassword(data) {
+  console.log('generatePassword: ', data);
+  
+  let availableMethods = [],
+  		password = '';
+  
+	if (data.includeLower) {
+  	availableMethods.push('lower');
+  }
+  
+  	if (data.includeUpper) {
+  	availableMethods.push('upper');
+  }
+
+  if (data.includeNumber) {
+  	availableMethods.push('number');
+  }
+  
+  if (data.includeSymbol) {
+  	availableMethods.push('symbol');
+  }
+  
+  if (availableMethods.length === 0) {
+  	password = 'Unable to generate password without a selection';
+  } else {
+  
+  	for (let i = 0; i < data.length; i++) {
+			let methodIdx = getRandomNumber(availableMethods.length),
+      		method = availableMethods[methodIdx],
+          char = randomCharacter[method]();
+      
+      console.log('Getting a [' + method + ']: ' + char);
+      
+      password += char;
+    }
+  }
+
+	return password;
+}
+
+//generate password eventListener
+generateButtonEl.addEventListener('click', () => {
+  passwordEl.value = generatePassword({
+    length: parseInt(lengthEl.value, 10),
+    includeLower: lowercaseEl.checked,
+    includeUpper: uppercaseEl.checked,
+    includeNumber: numberEl.checked,
+    includeSymbol: symbolsEl.checked
+  });
+});
